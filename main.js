470028a6ad5b26ee6236031273cd688de6d2d394
CarolLira <carol017lira@gmail.com>
2021-09-03 14:45:25 -0300

    :sparkles: Show 6 cards and create PlayerName component

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 import './styles/elements/base.scss';
 import './styles/generic/reset.scss';
 import CardGame from './src/components/CardGame';
+import PlayerName from './src/components/PlayerName';
+
+/* Apresentação dos 3 desafios do Módulo 01.
+1. Criar 6 CardGame na tela;
+2. Criar o component que representa o nome dos jogadores;
+3. Criar o verso do CardGame e a mecânica de virar a carta quando o player clicar nela. */
 
 const $app = document.querySelector('#app');
 const $htmlCardGame = CardGame();
+const cardsNumber = 6;
 
-$app.innerHTML = $htmlCardGame;
+function showCards(number) {
+    let i = 0, cards = '';
+    while (i <= number) {
+        cards += $htmlCardGame;
+        i++;
+    }
+    $app.innerHTML = cards;
+}
+
+showCards(cardsNumber);
+
