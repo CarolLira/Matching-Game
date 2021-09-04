8ba60aef8c2c200b47d4afbc71c0588a4b5b856d
CarolLira <carol017lira@gmail.com>
2021-09-04 18:26:15 -0300

    :construction: Create ScoreBoard component without style

--- main.js
+++ main.js
@@ -2,19 +2,13 @@ import './styles/generic/reset.scss';
 import './styles/elements/base.scss';
 
 import BoardGame from './src/objects/BoardGame';
-import PlayerName from './src/components/PlayerName';
-
-/* Apresentação dos 3 desafios do Módulo 01.
-1. Criar 6 CardGame na tela;
-2. Criar o component que representa o nome dos jogadores;
-3. Criar o verso do CardGame e a mecânica de virar a carta quando o player clicar nela. */
+import ScoreBoard from './src/objects/ScoreBoard';
 
 const $app = document.querySelector('#app');
 
 $app.innerHTML = 
 `
-    ${PlayerName('Player 1')}
-    ${PlayerName('Player 2')}
+    ${ScoreBoard()}
     ${BoardGame(6)}
 `;
 
